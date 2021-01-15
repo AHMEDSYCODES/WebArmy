c6c2cfdbd86397704289fd4dfafcc9096485aba7
AHMEDSYCODES <54114052+AHMEDSYCODES@users.noreply.github.com>
2021-01-15 18:38:58 +0100

    Add files via upload
    Anyone who can help me find error with the script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var x = document.getElementById("login");
+var y = doucment.getElementById("register");
+var z = document.getElementById("btn");
+        
+        function register(){
+            x.style.left = "-400px";
+            y.style.left = "50px";
+            z.style.left = "110px";
+        }
+        function login(){
+            x.style.left = "50px";
+            y.style.left = "50px";
+            z.style.left = "0px";
+        }
